4a9cd7177154d548b1a92387e7900821021260e3
PauloPortofolio <pvba02@gmail.com>
2023-01-29 16:04:28 -0500

    5.5) Exceptions
    -> CONCLUDED:
    Test MEssages improvements: enum border-line-styles as method param, format border-style dynamic
    
    -> WORKING:
    Test MEssages improvements: creating a dynamicLineGenerator
    GlobalExceptionHandler fix
    CustomExceptions Fix
    Fix the tests in Controller: Bugged after custom exceptions
    
    -> NEXT STEPS:
    Create specific tests for exceptions (file already done - use StepVerifier)
    Service tests: Fix it.
    Controller Test JsonSchema Bugged
    - www.anycodings.com/1questions/1523063/restassured-javalangillegalargumentexception-schema-to-use-cannot-be-null
    
    -> FUTURE IMPLEMENTATIONS:
    Docker Support
    CI/CD Support with deploy in AWS (use git-actions)
    REdirect WebApp for the domain fastappxxxx.com
    MAven Custom-Dynamic-ArchType generate (Save it in New-specific GitHub-Repo)

--- src/test/java/caching/config/utils/TestUtils.java
+++ src/test/java/caching/config/utils/TestUtils.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.testcontainers.containers.DockerComposeContainer;
 import org.testcontainers.containers.MongoDBContainer;
 
+import java.util.ArrayList;
 import java.util.stream.Stream;
 
 import static caching.config.utils.RestAssureSpecs.requestSpecs;
@@ -90,9 +91,7 @@ public class TestUtils {
       System.out.printf(
 
            "╔═══════════════════════════════════════════════════════════════════════\n" + "║     "
-           + "                      %s                        ║\n" + "║ --> Service: %s\n" + "║ " +
-           "--> Host: %s\n" + "║ --> Port: %s\n" + "║ --> Created: %s\n" + "║ --> Running:" + " " +
-           "%s\n" + "╚═══════════════════════════════════════════════════════════════════════\n\n",
+           + "                      %s                        ║\n" + "║ --> Service: %s\n" + "║ " + "--> Host: %s\n" + "║ --> Port: %s\n" + "║ --> Created: %s\n" + "║ --> Running:" + " " + "%s\n" + "╚═══════════════════════════════════════════════════════════════════════\n\n",
            "TC-CONTAINER-COMPOSE", service, compose.getServiceHost(service, port),
            compose.getServicePort(service, port), compose.getContainerByServiceName(service + "_1")
                                                          .get()
@@ -108,69 +107,34 @@ public class TestUtils {
 
     public static void main(String[] args) {
 
-      fullPanel(
-           21,
-           5,
-           3,
-           3,
-           true,
-           true,
-           "myTitcc cccc   cccc ccle dddddd", "myBoxxxdy", "myBvvy2", "myBvvy2"
+      mainPanel(21, 5, 3, 3, true, true, "myTitcc cccc   cccc ccle dddddd", "myBoxxxdy", "myBvvy2",
+                "myBvvy2"
       );
     }
 
     public static void simplePanel(String... texts) {
 
-      fullPanel(
-           21,
-           5,
-           1,
-           1,
-           true,
-           true,
-           texts
-      );
+      mainPanel(21, 5, 1, 1, true, true, texts);
     }
 
     public static void simplePanelWithScale(int scale, String... texts) {
 
-      fullPanel(
-           scale,
-           5,
-           1,
-           1,
-           true,
-           true,
-           texts
-      );
+      mainPanel(scale, 5, 1, 1, true, true, texts);
     }
 
-    public static void fullPanel(
-         int scale,
-         int margin,
-         int upSpace,
-         int downSpace,
-         boolean uppercaseTitle,
-         boolean centralizeTitle,
-         String... titleAndOthers) {
+    public static void mainPanel(int scale, int margin, int upSpace, int downSpace,
+                                 boolean uppercaseTitle, boolean centralizeTitle,
+                                 String... titleAndOthers) {
 
       var estimatedAdjustmentFactor = 3;
       var title = titleAndOthers[0];
       var marginTitle = scale - (title.length() / 2) - estimatedAdjustmentFactor;
-      var formattedTexts =
-           Stream
-                .of(titleAndOthers)
-                .map(item ->
-                          item.equals(title) &&
-                          centralizeTitle ?
-                               " ".repeat(marginTitle) + title :
-                               item)
-                .map(item ->
-                          item.equals(title) &&
-                          uppercaseTitle ?
-                               item.toUpperCase() :
-                               item)
-                .toArray();
+      var formattedTexts = Stream.of(titleAndOthers)
+                                 .map(item -> item.equals(title) && centralizeTitle ? " ".repeat(
+                                      marginTitle) + title : item)
+                                 .map(item -> item.equals(
+                                      title) && uppercaseTitle ? item.toUpperCase() : item)
+                                 .toArray();
 
       var marginLimitedBySize = Math.min(margin, scale);
 
@@ -187,151 +151,138 @@ public class TestUtils {
       var marginAsWhitespaces = " ".repeat(marginLimitedBySize);
       var upperExternalSpaces = "\n".repeat(upSpace);
       var bottomExternalSpaces = "\n".repeat(downSpace);
-      var drawline =
-           "_".repeat(scale)
-              .replace('_', MixedStyle.BASE_LINE.code);
+      var baseline = "_".repeat(scale)
+                        .replace('_', ThinFont.BASE_LINE.code);
 
-      var divider =
-           "_".repeat(scale)
-              .replace('_', MixedStyle.BASE_LINE_BOLD.code);
+      var divider = "_".repeat(scale)
+                       .replace('_', BoldFont.BASE_LINE.code);
 
-      var upperBorderLine = upperLineGenerator(drawline, BorderStyle.MIXED);
-      var middleBorderLine = middleLineGenerator(divider, BorderStyle.MIXED);
-      var bottomBorderLine = bottomLineGenerator(drawline, BorderStyle.MIXED);
+      var upperBorder = upperLine(baseline, BorderStyle.DOUBLE, BorderStyle.BOLD);
+      var dividerBorder = middleLine(divider, BorderStyle.DOUBLE, BorderStyle.DOUBLE);
+      var bottomBorder = bottomLine(baseline, BorderStyle.DOUBLE, BorderStyle.BOLD);
 
       var builder = new StringBuilder();
-      builder
-           .append(upperExternalSpaces)
-           .append(upperBorderLine)
-           .append(MixedStyle.MIDDLE_FACE.code)
-           .append("%s%%-%ss".formatted(marginAsWhitespaces, internalTextArea))
-           .append(MixedStyle.MIDDLE_FACE.code)
-           .append("\n")
-           .append(middleBorderLine)
-      ;
+      builder.append(upperExternalSpaces)
+             .append(upperBorder)
+             .append(ThinFont.FACE_LINE.code)
+             .append("%s%%-%ss".formatted(marginAsWhitespaces, internalTextArea))
+             .append(ThinFont.FACE_LINE.code)
+             .append("\n")
+             .append(dividerBorder);
 
       // "-1" Because the first element in the Array was used as title
       for (int i = formattedTexts.length - 1; i > 0; i--)
-        builder
-             .append(MixedStyle.MIDDLE_FACE.code)
-             .append("%s%%-%ss".formatted(marginAsWhitespaces, internalTextArea))
-             .append(MixedStyle.MIDDLE_FACE.code)
-             .append("\n");
+        builder.append(ThinFont.FACE_LINE.code)
+               .append("%s%%-%ss".formatted(marginAsWhitespaces, internalTextArea))
+               .append(ThinFont.FACE_LINE.code)
+               .append("\n");
 
-      builder
-           .append(bottomBorderLine)
-           .append(bottomExternalSpaces);
+      builder.append(bottomBorder)
+             .append(bottomExternalSpaces);
       System.out.printf(builder.toString(), formattedTexts);
-      xxxxx(bottomBorderLine,BorderStyle.BOLD);
     }
+
 
     @NotNull
-    private static String bottomLineGenerator(String baseline, BorderStyle style) {
+    private static String upperLine(
+         String baseline,
+         BorderStyle corner,
+         BorderStyle centerMark) {
 
-      return
-           MixedStyle.LOWER_LEFT_CORNER.code + baseline +
-           MixedStyle.MIDDLE_CENTER.code + baseline +
-           MixedStyle.LOWER_RIGHT_CORNER.code + "\n";
-    }
-
-    @NotNull
-    private static String middleLineGenerator(String divider, BorderStyle style) {
-
-      return
-           MixedStyle.MIDDLE_LEFT.code + divider +
-           MixedStyle.MIDDLE_CENTER.code + divider +
-           MixedStyle.MIDDLE_RIGHT.code + "\n";
-    }
-
-    @NotNull
-    private static String upperLineGenerator(String baseline, BorderStyle style) {
-
-      //      var xx = switch (style) {
-      //        BOLD -> boldStyle;
-      //        SLIM -> simpleStyle;
-      //        DOUBLE -> doubleStyle
-      //        default -> mixedStyle;
-      //      };
-
-      return
-           MixedStyle.UPPER_LEFT_CORNER.code + baseline +
-           MixedStyle.MIDDLE_CENTER.code + baseline +
-           MixedStyle.UPPER_RIGHT_CORNER.code + "\n";
-    }
-
-    @NotNull
-    private static String xxxxx(String baseline, BorderStyle xx) {
-
-      System.out.println(xx);
-
-      String hhh = switch (xx) {
-        BOLD -> "BoldStyle";
-        SLIM -> "SimpleStyle";
-        DOUBLE -> "DoubleStyle";
-        MIXED -> "DoubleStyle";
-        default -> throw new IllegalStateException("Invalid day: ");
-      };
-
-      return
-           MixedStyle.UPPER_LEFT_CORNER.code + baseline +
-           MixedStyle.MIDDLE_CENTER.code + baseline +
-           MixedStyle.UPPER_RIGHT_CORNER.code + "\n";
-    }
-
-    private static String simpleLineStyle(String str) {
-
-      return str.replace('a', '\u250c')
-                .replace('b', '\u252c')
-                .replace('c', '\u2510')
-                .replace('d', '\u251c')
-                .replace('e', '\u253c')
-                .replace('f', '\u2524')
-                .replace('g', '\u2514')
-                .replace('h', '\u2534')
-                .replace('i', '\u2518')
-                .replace('_', '\u2500')
-                .replace('|', '\u2502');
-    }
-
-    private static String mixedLineStyle(String str) {
-      //source: https://en.wikipedia.org/wiki/Box-drawing_character
-      return str.replace('a', '\u250F')
-                .replace('b', '\u252c')
-                .replace('c', '\u2513')
-                .replace('d', '\u2523')
-                .replace('e', '\u253c')
-                .replace('f', '\u252B')
-                .replace('g', '\u2517')
-                .replace('h', '\u2534')
-                .replace('i', '\u251B')
-                .replace('-', '\u2501')
-                .replace('_', '\u2500')
-                .replace('*', '\u2501')
-                .replace('|', '\u2502');
-    }
-
-    private enum MixedStyle {
-
-      MIDDLE_CENTER('\u2501'),
-      BASE_LINE('\u2500'),
-      BASE_LINE_BOLD('\u2501'),
-      UPPER_LEFT_CORNER('\u250F'),
-      UPPER_RIGHT_CORNER('\u2513'),
-      MIDDLE_LEFT('\u2523'),
-      MIDDLE_RIGHT('\u252B'),
-      MIDDLE_FACE('\u2502'),
-      LOWER_LEFT_CORNER('\u2517'),
-      LOWER_RIGHT_CORNER('\u251B');
-
-      private final char code;
-
-      MixedStyle(char code) {
-
-        this.code = code;
+      ArrayList<Character> myChars = new ArrayList<>();
+      switch (corner) {
+        case BOLD -> {
+          myChars.add(BoldFont.UPPER_LEFT_CORNER.code);
+          myChars.add(BoldFont.UPPER_RIGHT_CORNER.code);
+        }
+        case THIN -> {
+          myChars.add(ThinFont.UPPER_LEFT_CORNER.code);
+          myChars.add(ThinFont.UPPER_RIGHT_CORNER.code);
+        }
+        case DOUBLE -> {
+          myChars.add(DoubleFont.UPPER_LEFT_CORNER.code);
+          myChars.add(DoubleFont.UPPER_RIGHT_CORNER.code);
+        }
       }
+
+      switch (centerMark) {
+        case BOLD -> myChars.add(BoldFont.BASE_LINE.code);
+        case THIN -> myChars.add(ThinFont.BASE_LINE.code);
+        case DOUBLE -> myChars.add(DoubleFont.BASE_LINE.code);
+      }
+
+      return myChars.get(0) + baseline +
+             myChars.get(2) + baseline +
+             myChars.get(1) + "\n";
     }
 
-    private enum BoldStyle {
+    @NotNull
+    private static String middleLine(
+         String baseline,
+         BorderStyle corner,
+         BorderStyle centerMark) {
+
+      ArrayList<Character> myChars = new ArrayList<>();
+      switch (corner) {
+        case BOLD -> {
+          myChars.add(BoldFont.MIDDLE_LEFT.code);
+          myChars.add(BoldFont.MIDDLE_RIGHT.code);
+        }
+        case THIN -> {
+          myChars.add(ThinFont.MIDDLE_LEFT.code);
+          myChars.add(ThinFont.MIDDLE_RIGHT.code);
+        }
+        case DOUBLE -> {
+          myChars.add(DoubleFont.MIDDLE_LEFT.code);
+          myChars.add(DoubleFont.MIDDLE_RIGHT.code);
+        }
+      }
+
+      switch (centerMark) {
+        case BOLD -> myChars.add(BoldFont.BASE_LINE.code);
+        case THIN -> myChars.add(ThinFont.BASE_LINE.code);
+        case DOUBLE -> myChars.add(DoubleFont.BASE_LINE.code);
+      }
+
+      return myChars.get(0) + baseline +
+             myChars.get(2) + baseline +
+             myChars.get(1) + "\n";
+    }
+
+    @NotNull
+    private static String bottomLine(
+         String baseline,
+         BorderStyle corner,
+         BorderStyle centerMark) {
+
+      ArrayList<Character> myChars = new ArrayList<>();
+      switch (corner) {
+        case BOLD -> {
+          myChars.add(BoldFont.LOWER_LEFT_CORNER.code);
+          myChars.add(BoldFont.LOWER_RIGHT_CORNER.code);
+        }
+        case THIN -> {
+          myChars.add(ThinFont.LOWER_LEFT_CORNER.code);
+          myChars.add(ThinFont.LOWER_RIGHT_CORNER.code);
+        }
+        case DOUBLE -> {
+          myChars.add(DoubleFont.LOWER_LEFT_CORNER.code);
+          myChars.add(DoubleFont.LOWER_RIGHT_CORNER.code);
+        }
+      }
+
+      switch (centerMark) {
+        case BOLD -> myChars.add(BoldFont.BASE_LINE.code);
+        case THIN -> myChars.add(ThinFont.BASE_LINE.code);
+        case DOUBLE -> myChars.add(DoubleFont.BASE_LINE.code);
+      }
+
+      return myChars.get(0) + baseline +
+             myChars.get(2) + baseline +
+             myChars.get(1) + "\n";
+    }
+
+    private enum BoldFont {
 
       FACE_LINE('\u2503'),
       BASE_LINE('\u2501'),
@@ -344,13 +295,13 @@ public class TestUtils {
 
       private final char code;
 
-      BoldStyle(char code) {
+      BoldFont(char code) {
 
         this.code = code;
       }
     }
 
-    private enum SimpleStyle {
+    private enum ThinFont {
 
       FACE_LINE('\u2502'),
       BASE_LINE('\u2500'),
@@ -363,13 +314,13 @@ public class TestUtils {
 
       private final char code;
 
-      SimpleStyle(char code) {
+      ThinFont(char code) {
 
         this.code = code;
       }
     }
 
-    private enum DoubleStyle {
+    private enum DoubleFont {
 
       FACE_LINE('\u2551'),
       BASE_LINE('\u2550'),
@@ -382,19 +333,47 @@ public class TestUtils {
 
       private final char code;
 
-      DoubleStyle(char code) {
+      DoubleFont(char code) {
 
         this.code = code;
       }
     }
 
     private enum BorderStyle {
-      BOLD,
-      MIXED,
-      SLIM,
-      DOUBLE;
+      BOLD, THIN, DOUBLE
     }
   }
 
 
 }
+//    private static String simpleLineStyle(String str) {
+//
+//      return str.replace('a', '\u250c')
+//                .replace('b', '\u252c')
+//                .replace('c', '\u2510')
+//                .replace('d', '\u251c')
+//                .replace('e', '\u253c')
+//                .replace('f', '\u2524')
+//                .replace('g', '\u2514')
+//                .replace('h', '\u2534')
+//                .replace('i', '\u2518')
+//                .replace('_', '\u2500')
+//                .replace('|', '\u2502');
+//    }
+//
+//    private static String mixedLineStyle(String str) {
+//      //source: https://en.wikipedia.org/wiki/Box-drawing_character
+//      return str.replace('a', '\u250F')
+//                .replace('b', '\u252c')
+//                .replace('c', '\u2513')
+//                .replace('d', '\u2523')
+//                .replace('e', '\u253c')
+//                .replace('f', '\u252B')
+//                .replace('g', '\u2517')
+//                .replace('h', '\u2534')
+//                .replace('i', '\u251B')
+//                .replace('-', '\u2501')
+//                .replace('_', '\u2500')
+//                .replace('*', '\u2501')
+//                .replace('|', '\u2502');
+//    }
